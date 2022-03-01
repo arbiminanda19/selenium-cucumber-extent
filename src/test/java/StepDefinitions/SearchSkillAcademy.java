7ad08457840da44271a724771b87c6dea91aae99
arbiminanda <arbiminanda10@gmail.com>
2022-03-01 21:18:26 +0700

    add search with set course amount per page

--- src/test/java/StepDefinitions/SearchSkillAcademy.java
+++ src/test/java/StepDefinitions/SearchSkillAcademy.java
@@ -6,6 +6,7 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.And;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -24,6 +25,7 @@ public class SearchSkillAcademy {
 	WebDriver driver = null;
 	Random rand = new Random();
 	String keyword = "SMA";
+	int amount = 0;
 	
 	@Given("User is on skillacademy homepage")
 	public void skilacademy_homepage() {
@@ -59,5 +61,39 @@ public class SearchSkillAcademy {
 	    	driver.findElement(By.xpath("//h2[contains(text(),'" + keyword.toLowerCase() + "')]"));
 	    }
 	    driver.close();
-	}	
+	}
+	
+	@When("User choose amount of result")
+	public void choose_amount() {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.or(
+		    ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-testid='pagination-per-page']"))
+		));
+		driver.findElement(By.xpath("//div[@data-testid='pagination-per-page']")).click();
+		List<Integer> listLanguage = Arrays.asList(10,20,50,100);
+		amount = listLanguage.get(rand.nextInt(listLanguage.size()));
+		if (amount == 10) {
+			driver.findElement(By.xpath("//div[@tabindex='-1'][contains(text(),'10')]")).click();
+		}
+		if (amount == 20) {
+			driver.findElement(By.xpath("//div[@tabindex='-1'][contains(text(),'20')]")).click();
+		}
+		if (amount == 50) {
+			driver.findElement(By.xpath("//div[@tabindex='-1'][contains(text(),'50')]")).click();
+		}
+		if (amount == 100) {
+			driver.findElement(By.xpath("//div[@tabindex='-1'][contains(text(),'100')]")).click();
+		}
+	}
+	
+	@Then("User see amount of search result match with amount that has been chosen")
+	public void amount_result_match() {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.or(
+			ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-testid='course-image']"))
+		));
+		List<WebElement> course_element = driver.findElements(By.xpath("//div[@data-testid='course-image']"));
+		assertEquals(amount, course_element.size());
+	    driver.close();
+	}
 }
--- src/test/resources/Feature/SearchSkillAcademy.feature
+++ src/test/resources/Feature/SearchSkillAcademy.feature
@@ -7,3 +7,11 @@ Feature: Search in SkillAcademy Web
 		When User fill search keyword
 		And User click search button
 		Then User see the search results that match with keyword
+		
+	@Test2	
+	Scenario: User search spesific keyword and change amount of searching result in one page
+		Given User is on skillacademy homepage
+		When User fill search keyword
+		And User click search button
+		When User choose amount of result
+		Then User see amount of search result match with amount that has been chosen
