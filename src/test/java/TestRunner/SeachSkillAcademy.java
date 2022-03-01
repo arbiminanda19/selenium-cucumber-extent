fcc99defdad9873b489bed62145fe9bd987ea45a
arbiminanda <arbiminanda10@gmail.com>
2022-03-01 23:00:54 +0700

    edit test runner

--- src/test/java/TestRunner/SeachSkillAcademy.java
+++ src/test/java/TestRunner/SeachSkillAcademy.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\resources\\Feature\\SearchSkillAcademy.feature", glue="StepDefinitions", monochrome=true, tags="@Test4",  
+@CucumberOptions(features="src\\test\\resources\\Feature\\SearchSkillAcademy.feature", glue="StepDefinitions", monochrome=true, tags="@All",  
 plugin={"pretty", "html:target/HtmlReports.html", "json:target/JSONReports/report.json", "junit:target/JUnitReports/report.xml"})
 
 public class SeachSkillAcademy {
